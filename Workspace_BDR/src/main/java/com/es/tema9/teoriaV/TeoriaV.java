3ea6b52c44f89cbd14cf8fe045bf16c7d4301a56
jroddom0103 <jroddom0103@g.educaand.es>
2024-05-20 14:28:28 +0200

    Añado

--- Workspace_BDR/src/main/java/com/es/tema9/teoriaV/TeoriaV.java
+++ Workspace_BDR/src/main/java/com/es/tema9/teoriaV/TeoriaV.java
@@ -31,6 +31,7 @@ public class TeoriaV {
             // C) Crear un objeto Statement con la QUERY
             PreparedStatement pst = conectarBDTv.obtenerConexion().prepareStatement(DBUtils.QUERY_LOGIN);
 
+
             // METER LOS PARAMETROS
             // (el número del parámetro, y el parámetro en sí)
             pst.setString(1,user);
@@ -42,6 +43,41 @@ public class TeoriaV {
             // E) LEER RESULTADOS
             if (rs.next()){
                 System.out.println("Bienvenid@.");
+
+
+                PreparedStatement pst2 = conectarBDTv.obtenerConexion().prepareStatement(DBUtils.QUERY_CHECK_USER_IN_ACCESOS);
+                pst2.setString(1,user);
+
+                ResultSet rs2 = pst2.executeQuery();
+                if (rs2.next()){
+                    PreparedStatement pst3 = conectarBDTv.obtenerConexion().prepareStatement(DBUtils.QUERY_UPDATE_NACCESOS);
+                    pst3.setString(1,user);
+                    int nfilas = pst3.executeUpdate();
+
+                    if (nfilas>0){
+                        System.out.println("Registro actualizado correctamente.");
+                    }else{
+                        System.out.println("ID no encontrado");
+                    }
+                }else{
+                    PreparedStatement pst4 = conectarBDTv.obtenerConexion().prepareStatement(DBUtils.QUERY_INSERT_USER_IN_ACCESOS);
+                    pst4.setString(1,user);
+                    pst4.setInt(2,0);
+                    int nfilas = pst4.executeUpdate();
+                    if (nfilas>0){
+                        System.out.println("Registro insertado correctamente.");
+                    }else{
+                        System.out.println("Fallo al insertar.");
+                    }
+                    PreparedStatement pst5 = conectarBDTv.obtenerConexion().prepareStatement(DBUtils.QUERY_UPDATE_NACCESOS);
+                    pst5.setString(1,user);
+                    int nfilasUpdate = pst5.executeUpdate();
+                    if (nfilasUpdate>0){
+                        System.out.println("Se actualizó el registro con éxito");
+                    }else{
+                        System.out.println("Hubo un fallo al actualizar");
+                    }
+                }
               }else{
                 System.out.println("No estás registrado.");
             }
--- Workspace_BDR/src/main/java/com/es/tema9/teoriaV/utils/DBUtils.java
+++ Workspace_BDR/src/main/java/com/es/tema9/teoriaV/utils/DBUtils.java
@@ -9,8 +9,8 @@ public class DBUtils {
 
     //QUERYS DE LA APLICACIÓN
     public static String QUERY_LOGIN = "SELECT * FROM usuarios WHERE id=? AND pass = ?;";
-    public static String QUERY_CHECK_USER_IN_ACCESOS = "SELECT * FROM accesos WHERE id = ?;";
+    public static String QUERY_CHECK_USER_IN_ACCESOS = "SELECT * FROM accesos WHERE idUsuario = ?;";
     public static String QUERY_INSERT_USER_IN_ACCESOS = "INSERT INTO accesos VALUES(?, ?);";
-    public static String QUERY_UPDATE_NACCESOS = "UPDATE accesos SET nAccesos = nAccesos+1 WHERE id = ?;";
+    public static String QUERY_UPDATE_NACCESOS = "UPDATE accesos SET nAccesos = nAccesos+1 WHERE idUsuario = ?;";
 
 }
